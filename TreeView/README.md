4dcfe162996d372cd594015ac55e5468107155f1
710605420@qq.com <710605420@qq.com>
2018-12-14 15:01:44 +0800

    添加treeview

---
+++ TreeView/README.md
@@ -0,0 +1,3 @@
+# TreeView
+UGUI树形菜单
+更多详细信息请观看此篇博客：http://blog.csdn.net/qq992817263/article/details/54925472
---
+++ TreeView/Script/TreeViewData.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// 树形菜单数据
+/// </summary>
+public class TreeViewData
+{
+    /// <summary>
+    /// 数据内容
+    /// </summary>
+    public string Name;
+    /// <summary>
+    /// 数据所属的父ID
+    /// </summary>
+    public int ParentID;
+    /// <summary>
+    /// 数据自己ID
+    /// </summary>
+    public int SelfID;
+
+}
